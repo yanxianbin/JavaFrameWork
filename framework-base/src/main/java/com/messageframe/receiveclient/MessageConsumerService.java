820554faed983203c44732861153687a2b1cfe28
yanxianbin <yanxianbin@ky-tech.com.cn>
2020-01-04 11:17:38 +0800

    加入基础的启动信息

--- framework-base/src/main/java/com/messageframe/receiveclient/MessageConsumerService.java
+++ framework-base/src/main/java/com/messageframe/receiveclient/MessageConsumerService.java
@@ -1,17 +1,22 @@
 package com.messageframe.receiveclient;
 
-import com.constants.ApplicationException;
+import com.constants.MessageConsumerType;
 import com.constants.MessageStateEnum;
 import com.messageframe.mode.ReceiveMode;
 import com.messageframe.service.ReceiveMessageService;
 import com.startup.InitializationMqListener;
 import com.utils.CommonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * @Classname MessageConsumerService
@@ -25,6 +30,11 @@ public class MessageConsumerService {
     @Autowired
     private ReceiveMessageService receiveService;
 
+    /**
+     * 消费消息
+     * @param message
+     * @return
+     */
     public boolean consumer(ReceiveMode message){
         boolean isSuccess = false;
         ReceiveMode entity = receiveService.findById(message.getId());
@@ -36,7 +46,8 @@ public class MessageConsumerService {
                 entity.setFailReason(String.format("业务类型：%s 客户端未注册", message.getBusinessType()));
             } else {
                 try {
-                    isSuccess = client.execute(message.getMessage());
+                    //调用对应的消费者
+                    isSuccess = executeMessage(message,client);
                 } catch (Exception ex) {
                     entity.setFailReason(CommonUtils.getExceptionMsg(ex));
                 }
@@ -50,7 +61,66 @@ public class MessageConsumerService {
       return isSuccess;
     }
 
-//    private List<ReceiveClient> scanClient(){
-//
-//    }
+    private void updateMessageState(ReceiveMode message,ReceiveClient client){
+        if(client.businessType().equals(MessageConsumerType.LAST)){
+
+        }
+    }
+
+    /**
+     * 调用消费者
+     * @param message
+     * @param client
+     * @return
+     */
+    private boolean executeMessage(ReceiveMode message,ReceiveClient client){
+        Boolean bl=false;
+        if(client.businessType().equals(MessageConsumerType.LAST)){
+           String lastMessage= getLastMessage(message.getBusinessNumber(),message.getBusinessType());
+           bl=client.execute(lastMessage);
+        }else if(client.businessType().equals(MessageConsumerType.LIST)){
+            List<String> messageList=sortList(message.getBusinessNumber(),message.getBusinessType());
+            for(String sortMessage : messageList){
+                bl=client.execute(sortMessage);
+            }
+        }else{
+            client.execute(message.getMessage());
+        }
+        return bl;
+    }
+
+    /**
+     * 按照顺序回放
+     * @param businessNumber
+     * @param businessType
+     * @return
+     */
+    private List<String> sortList(String businessNumber,String businessType){
+        Criteria criteria=Criteria.where("businessNumber").is(businessNumber).and("businessType").is(businessType);
+        Query query=new Query(criteria);
+        query.with(Sort.by(new Sort.Order(Sort.Direction.ASC,"createTime")));
+        List<ReceiveMode> receiveModeList= receiveService.find(query);
+        if(CollectionUtils.isEmpty(receiveModeList)){
+            return receiveModeList.stream().map(x->x.getMessage()).collect(Collectors.toList());
+        }
+        return null;
+    }
+
+    /**
+     * 获取最后一条记录
+     * @param businessNumber
+     * @param businessType
+     * @return
+     */
+    private String getLastMessage(String businessNumber,String businessType){
+        Criteria criteria=Criteria.where("businessNumber").is(businessNumber).and("businessType").is(businessType);
+        Query query=new Query(criteria);
+        query.with(Sort.by(new Sort.Order(Sort.Direction.DESC,"createTime")));
+        query.limit(1);
+        List<ReceiveMode> receiveModeList= receiveService.find(query);
+        if(CollectionUtils.isEmpty(receiveModeList)){
+            return receiveModeList.get(0).getMessage();
+        }
+        return null;
+    }
 }
